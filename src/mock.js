bd39ccb7262996b6504094b6c6a6cc4a6ee7cd70
= <westerovs@mail.ru>
2021-07-31 18:28:28 +0300

    added template mock task

---
+++ src/mock.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+
+const getRandomName = () => {
+
+}
+
+const getRandomPoster = () => {
+
+}
+
+const getRandomDescription = () => {
+
+}
+
+const getRandomEmoji = () => {
+
+}
+
+const getRandomDate = () => {
+
+}
+
+const getRandomAuthor = () => {
+
+}
+
+
+const task = {
+  name: getRandomName(),
+  poster: getRandomPoster(),
+  description: getRandomDescription(),
+  comments: {
+    emoji: getRandomEmoji(),
+    date: getRandomDate(),
+    author: getRandomAuthor(),
+    description: getRandomDescription(),
+  }
+}
