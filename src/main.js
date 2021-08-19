03af1c2d851db3619113301e1d21486f6209db3c
Александр <westerovs@mail.ru>
2021-08-19 22:24:53 +0300

    4.2 added Abstract class - добавил абстрактный класс и методы, которые понадобятся дочерним компонентам

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { generateFilm } from './mock/film.js';
 import { toFiltersCount } from './mock/filter.js';
 import { render } from './utils/utils.js';
 
+import Abstract from './view/abstract.js';
 import RankView from './view/rank.js';
 import FilterView from './view/filter.js';
 import SortingView from './view/sort.js';
---
+++ src/view/abstract.js
@@ -0,0 +1,28 @@
+import { createElement } from '../utils/utils.js';
+
+class Abstract {
+  constructor() {
+    if (new.target === Abstract) {
+      throw Error('Нельзя создать экземпляр класса. Только наследование !');
+    }
+
+    this._elem = null;
+  }
+
+  getTemplate(element) {
+    this._elem = element;
+  }
+
+  getElement() {
+    if (!this._elem) {
+      this._elem = createElement(this.getTemplate());
+    }
+
+    return this._elem;
+  }
+
+  remove() {
+    this._elem = null;
+  }
+}
+
